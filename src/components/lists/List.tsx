f7e7d5a731b50da71ad5fce0d5cac05380c052a4
DominikFrackowiak <rorschach@op.pl>
2024-07-18 12:38:12 +0200

    update

--- src/components/lists/List.tsx
+++ src/components/lists/List.tsx
@@ -2,11 +2,10 @@ import { Author } from '../../Types'
 
 interface Props {
 	items: Author[]
-	sourceName: string
 	ItemListComponent: React.ComponentType<{ author: Author }>
 }
 
-export default function List({ items, sourceName, ItemListComponent }: Props) {
+export default function List({ items, ItemListComponent }: Props) {
 	return (
 		<>
 			{items.map((item, i) => (
